98c6b5a216da229ff0ec650554761188052daf0e
ranjitkokare <ranjitkokare999@gmail.com>
2025-06-19 13:18:41 +0530

    Add appraoch_1 to access configuration buildVersion

---
+++ section_6/v1-springboot/accounts/src/main/java/com/eazybytes/accounts/repository/AccountsRepository.java
@@ -0,0 +1,23 @@
+package com.eazybytes.accounts.repository;
+
+import com.eazybytes.accounts.entity.Accounts;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+
+    Optional<Accounts> findByCustomerId(Long customerId);
+
+//    Whenever you update or delete data in the database with custom query methods,
+//    you need to use 2 annotations:
+//    @Transactional and @Modifying annotation
+
+    @Transactional //(If error happen then rollback)
+    @Modifying
+    void deleteByCustomerId(Long customerId);
+}
